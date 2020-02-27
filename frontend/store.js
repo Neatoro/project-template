78b12a1cc89b32b119f0b5d452eb90958ebbf566
Moritz Schramm <moritz.schramm@gmx.de>
2020-02-27 10:45:44 +0100

    Initial commit

---
+++ frontend/store.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const actionTypes = {};
+export const mutationTypes = {};
+
+export default new Vuex.Store({
+  state: {},
+  actions: {},
+  mutations: {}
+});
